09d7262d937c23ab5fba30aefa464f29b2ed86e5
Frotaur <frotaut@hotmail.co.uk>
2023-11-01 17:24:52 +0100

    Training works, different fitnesses

--- GeneticOptimizer.py
+++ GeneticOptimizer.py
@@ -19,8 +19,7 @@ class GeneticOptimizer:
         incoming_rate : percentage of new population to be added to children
     """
 
-    def __init__(self,size,square_size,population_size,simulation_steps,
-                 survival_rate,incoming_rate,device='cpu'):
+    def __init__(self,size,square_size,population_size,simulation_steps,mutation_rate=0.03,device='cpu'):
         self.automaton = VonNeumann(size,device=device)
 
         self.population_size = population_size
@@ -28,6 +27,7 @@ class GeneticOptimizer:
 
         self.size = size
         self.square_size = square_size
+        self.square_mask = self.get_square_mask()
 
         self.initial_excitation = (torch.rand(self.automaton.excitations.shape)<0.5).to(dtype=torch.uint8).to(self.automaton.device)
         torch.save(self.initial_excitation,'initial_excitation.pt')
@@ -36,12 +36,12 @@ class GeneticOptimizer:
             self.states.append(self.get_random_state())
         self.states = [self.get_random_state() for _ in range(population_size)]
 
+        self.mutation_rate = mutation_rate
+
         self.device = device
     
 
-    def get_random_state(self):
-        self.automaton.set_state(self.automaton.get_rando_para_state(0.3,0.3,0.3))
-        # self.automaton.run_mcmc(1,2,replace_prob=0.1)
+    def get_square_mask(self):
         H,W = self.size
         h,w = self.square_size
 
@@ -58,7 +58,34 @@ class GeneticOptimizer:
         # Create the mask based on the condition
         mask = ((xx >= start_w) & (xx < end_w) & (yy >= start_h) & (yy < end_h)).float()
 
-        return self.automaton.get_state()*mask[None,:,:]
+        return mask
+    
+    def get_random_state(self):
+        self.automaton.set_state(self.automaton.get_rando_para_state(0.3,0.3,0.3))
+        # self.automaton.run_mcmc(1,2,replace_prob=0.1)
+
+        return self.automaton.get_state()*self.square_mask[None,:,:]
+    
+    def fitness_mover(self,state):
+        eval_period = 20
+
+        self.automaton.set_state(state)
+        for _ in range(self.simulation_steps):
+            self.automaton.step()
+        
+        excitation_table = []
+        for _ in range(eval_period):
+            self.automaton.step()
+            excitation_table.append(self.automaton.excitations.clone())
+
+        excitation_tensor = torch.stack(excitation_table,dim=0) # (eval_period,H,W)
+        score_tensor = torch.zeros_like(excitation_tensor) # (eval_period,H,W)
+        for i in range(1, eval_period - 1):
+            max_before = excitation_tensor[:i, :, :].max(dim=0)[0]
+            max_after = excitation_tensor[i:, :, :].max(dim=0)[0]
+            score_tensor += (max_after - max_before).clamp(min=0)
+
+        return score_tensor.mean().item()
     
     def fitness_num_states_0(self,state):
 
@@ -69,24 +96,44 @@ class GeneticOptimizer:
         for _ in range(self.simulation_steps):
             self.automaton.step()
         
-        self.automaton.compute_is_ground()
-        # showTens(self.automaton.is_ground)
+        # self.automaton.compute_is_ground()
 
-        return (1-self.automaton.is_ground).to(dtype=torch.float32).mean().item()
+        return (self.automaton.is_conf).to(dtype=torch.float32).mean().item()
+
+    def repro_mask(self, num_centers=8, r=None):
+        """
+            Compute a 0/1 mask with big chunks, that determines from which parent the child state 
+            will inherit the pixel.
+
+            returns a (1, h, w) tensor
+        """
+        h,w = self.size
+
+        if r is None: r = (w + h) / 10 # a bit hacky
+        xx, yy = torch.meshgrid(torch.linspace(0, w - 1, w), torch.linspace(0, h - 1, h), indexing='xy')
+        field = torch.zeros(w, h)
+        for i in range(num_centers):
+            cx, cy = random.random() * w, random.random() * h
+            sqxx, sqyy = (xx - cx) ** 2, (yy - cy) ** 2
+            sqd = sqxx + sqyy
+            field += (2 * random.random() - 1) * torch.exp(-sqd / (r ** 2))
+
+        return field>=0. # [h, w]
 
     def generate_child(self,parent1,parent2):
         """
             Generates a child from two parents.
             The child is a random mix of the two parents.
         """
-        if(torch.rand(1,device=self.device)<0.5):
-            # 50% chance of reproduction
-            child_state = torch.where(torch.linspace(0,1,self.size[0],device=self.device)[None,None,:]<0.5,parent1,parent2)
-            # 3% mutations
-            child_state = torch.where(torch.rand(self.size,device=self.device)<0.03,self.get_random_state(),child_state)
-            return child_state
-        else:
-            return parent1
+
+        # reproduction by crossover
+        child_state = torch.where(torch.full(parent1.shape,True),parent1,parent2)
+        
+        # Mutations
+        child_state = torch.where(torch.rand(self.size,device=self.device)<self.mutation_rate,self.get_random_state(),child_state)
+       
+        return child_state
+
 
     def draw_state(self, state):
         self.automaton.set_state(state)
@@ -95,14 +142,17 @@ class GeneticOptimizer:
         showTens(self.automaton.torch_worldmap)
     
     def evolve(self,num_generations):
+        num_survive = int(self.population_size*0.1)
+        num_reprod = int(self.population_size*0.2)
+        num_children = self.population_size-num_survive
         for k in range(num_generations):
             t1 = time.time()
             fitnesses = []
             for state in tqdm(self.states):
                 fitnesses.append(self.fitness_num_states_0(state))
         
-            print(f'fitness : {time.time()-t1}s')
-            thresh_fitness = np.percentile(fitnesses,75)
+            sorted_indices = np.argsort(fitnesses)[::-1]
+
             mean_fitness = statistics.mean(fitnesses)
             max_index = fitnesses.index(max(fitnesses))
 
@@ -110,14 +160,17 @@ class GeneticOptimizer:
                 self.draw_state(self.states[max_index])
             torch.save(self.states[max_index],'best_state.pt')
 
-            print(f'Gen {k} : {thresh_fitness:}, {mean_fitness:}''Mean fitness: ',mean_fitness,' Median fitness: ',thresh_fitness)
-            t1 = time.time()
-            surviving_states = [state for (state, fitness) in zip(self.states, fitnesses) if fitness >= thresh_fitness]
-            next_states =[self.generate_child(random.choice(surviving_states),random.choice(surviving_states)) for _ in range(self.population_size)]
-            self.states = next_states
-            print(f'evolve : {time.time()-t1}s')
+            print(f'Gen {k} : {mean_fitness:}''Mean fitness: ',mean_fitness)
+
+            surviving_states = [self.states[k] for k in sorted_indices[:num_survive]]
+            reproducing_states = [self.states[k] for k in sorted_indices[:num_reprod]]
+
+            next_child_states =[self.generate_child(random.choice(reproducing_states),random.choice(reproducing_states)) for _ in range(num_children)]
+            
+            self.states = surviving_states+next_child_states
+            assert len(self.states)==self.population_size
 
 if __name__=='__main__':
-    geno = GeneticOptimizer((100,100),(30,30),40,150,0.5,0.5,device='cpu')
+    geno = GeneticOptimizer((100,100),(30,30),40,150,0.03,device='cpu')
 
     geno.evolve(100)
